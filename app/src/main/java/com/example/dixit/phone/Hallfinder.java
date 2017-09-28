824a2ce87b20016ebaaa8e6a395264a176ff5274
Dixit Bishwas <dkbishwash@gmail.com>
2017-09-28 22:48:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dixit/phone/Hallfinder.java
@@ -0,0 +1,13 @@
+package com.example.dixit.phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hallfinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallfinder);
+    }
+}
